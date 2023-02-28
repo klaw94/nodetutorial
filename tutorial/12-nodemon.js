7dbd936260bc6f92163e634788e560d7dff8479b
clausaji <c.sanchezjimenez@qquest.nl>
2023-02-28 09:51:50 +0100

    I added some things

---
+++ tutorial/12-nodemon.js
@@ -0,0 +1,8 @@
+//nodemon is a tool that runs your code and everytime you save something he just reruns it, 
+//so you dont have to be doing node app.js every time.
+
+//We install it as a devDependency with `npm i nodemon --save-dev` because we only use it for dev. When
+//our app is deployed in production we will use something else. 
+
+//In the package.json "scripts" lets you give commands to your app. So "start" : "node app.js", so when you run npm start
+//you are running node app.js
